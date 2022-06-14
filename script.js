7322ddc97e6e955ca1bdcc23e930a69ec679f68d
Elmar099 <elmar6999@gmail.com>
2022-06-13 17:22:41 -0700

    added new buttons

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-document.getElementById("number").innerHTML = 
-Math.floor(Math.random() * 100);
+let rand = [Math.floor(Math.random() * 1000)];
+console.log(rand);
+let up = document.getElementById('give');
+let btn = document.getElementById('submit');
+let out = document.getElementById('output');
+
+btn.addEventListener('click', function(){
+    let input = document.getElementById('userInput').value;
+    if(input == rand) {
+        out.innerHTML = `Congrats! You guessed right!`
+    } else if(input < rand) {
+        out.innerHTML = "Go Higher!"
+    } if (input > rand){
+        out.innerHTML = "Go Lower!"
+    }
+})
+
+up.addEventListener('click', function() {
+    out.innerHTML = `You gave up that easily? you number was ${rand}`
+})
